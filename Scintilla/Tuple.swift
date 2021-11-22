d59e740bbdcb634339d904d0872b959e6e63a924
quephird <danielle_kefford@hotmail.com>
2021-11-21 20:42:52 -0800

    Settled implementations of Matrix4 and Tuple4.

--- Scintilla/Tuple.swift
+++ Scintilla/Tuple.swift
@@ -7,47 +7,58 @@
 
 import Foundation
 
-typealias Tuple = Array<Double>
-
 struct Tuple4 {
-    var xyzw: (Double, Double, Double, Double)
+    var xyzw: [Double]
 
     init(_ x: Double, _ y: Double, _ z: Double, _ w: Double) {
-        self.xyzw = (x, y, z, w)
+        self.xyzw = [x, y, z, w]
     }
 
     func add(_ other: Self) -> Self {
         Tuple4(
-            self.xyzw.0+other.xyzw.0,
-            self.xyzw.1+other.xyzw.1,
-            self.xyzw.2+other.xyzw.2,
-            self.xyzw.3+other.xyzw.3
+            self.xyzw[0]+other.xyzw[0],
+            self.xyzw[1]+other.xyzw[1],
+            self.xyzw[2]+other.xyzw[2],
+            self.xyzw[3]+other.xyzw[3]
         )
     }
 
     func subtract(_ other: Self) -> Self {
         Tuple4(
-            self.xyzw.0-other.xyzw.0,
-            self.xyzw.1-other.xyzw.1,
-            self.xyzw.2-other.xyzw.2,
-            self.xyzw.3-other.xyzw.3
+            self.xyzw[0]-other.xyzw[0],
+            self.xyzw[1]-other.xyzw[1],
+            self.xyzw[2]-other.xyzw[2],
+            self.xyzw[3]-other.xyzw[3]
         )
     }
 
     func negate() -> Self {
-        Tuple4(-self.xyzw.0, -self.xyzw.1, -self.xyzw.2, -self.xyzw.3)
+        Tuple4(-self.xyzw[0], -self.xyzw[1], -self.xyzw[2], -self.xyzw[3])
     }
 
     func multiplyScalar(_ scalar: Double) -> Self {
-        Tuple4(scalar*self.xyzw.0, scalar*self.xyzw.1, scalar*self.xyzw.2, scalar*self.xyzw.3)
+        Tuple4(
+            scalar*self.xyzw[0],
+            scalar*self.xyzw[1],
+            scalar*self.xyzw[2],
+            scalar*self.xyzw[3]
+        )
     }
 
     func divideScalar(_ scalar: Double) -> Self {
-        Tuple4(self.xyzw.0/scalar, self.xyzw.1/scalar, self.xyzw.2/scalar, self.xyzw.3/scalar)
+        Tuple4(
+            self.xyzw[0]/scalar,
+            self.xyzw[1]/scalar,
+            self.xyzw[2]/scalar,
+            self.xyzw[3]/scalar
+        )
     }
 
     func magnitude() -> Double {
-        (self.xyzw.0*self.xyzw.0 + self.xyzw.1*self.xyzw.1 + self.xyzw.2*self.xyzw.2 + self.xyzw.3*self.xyzw.3).squareRoot()
+        (self.xyzw[0]*self.xyzw[0] +
+            self.xyzw[1]*self.xyzw[1] +
+            self.xyzw[2]*self.xyzw[2] +
+            self.xyzw[3]*self.xyzw[3]).squareRoot()
     }
 
     func normalize() -> Self {
@@ -55,22 +66,25 @@ struct Tuple4 {
     }
 
     func dot(_ other: Self) -> Double {
-        self.xyzw.0*other.xyzw.0 + self.xyzw.1*other.xyzw.1 + self.xyzw.2*other.xyzw.2 + self.xyzw.3*other.xyzw.3
+        self.xyzw[0]*other.xyzw[0] +
+            self.xyzw[1]*other.xyzw[1] +
+            self.xyzw[2]*other.xyzw[2] +
+            self.xyzw[3]*other.xyzw[3]
     }
 
     func cross(_ other: Self) -> Self {
         vector(
-            self.xyzw.1 * other.xyzw.2 - self.xyzw.2 * other.xyzw.1,
-            self.xyzw.2 * other.xyzw.0 - self.xyzw.0 * other.xyzw.2,
-            self.xyzw.0 * other.xyzw.1 - self.xyzw.1 * other.xyzw.0
+            self.xyzw[1] * other.xyzw[2] - self.xyzw[2] * other.xyzw[1],
+            self.xyzw[2] * other.xyzw[0] - self.xyzw[0] * other.xyzw[2],
+            self.xyzw[0] * other.xyzw[1] - self.xyzw[1] * other.xyzw[0]
         )
     }
 
     func isAlmostEqual(_ to: Self) -> Bool {
-        self.xyzw.0.isAlmostEqual(to.xyzw.0) &&
-            self.xyzw.1.isAlmostEqual(to.xyzw.1) &&
-            self.xyzw.2.isAlmostEqual(to.xyzw.2) &&
-            self.xyzw.3.isAlmostEqual(to.xyzw.3)
+        self.xyzw[0].isAlmostEqual(to.xyzw[0]) &&
+            self.xyzw[1].isAlmostEqual(to.xyzw[1]) &&
+            self.xyzw[2].isAlmostEqual(to.xyzw[2]) &&
+            self.xyzw[3].isAlmostEqual(to.xyzw[3])
     }
 }
 
